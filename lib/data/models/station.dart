ae68e2fcc70ce99f437037dc50c302312821d8f1
flexagoon <code@fxgn.dev>
2023-08-24 15:29:44 +0300

    make station immutable

--- lib/data/models/station.dart
+++ lib/data/models/station.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
+
+@immutable
 final class Station {
   final String name;
   final int line;
 
-  Station({
+  const Station({
     required this.name,
     required this.line,
   });

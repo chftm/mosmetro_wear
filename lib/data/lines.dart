e289f8cd0bf90fe7402dbdfe9f86b7cbde5268b9
flexagoon <code@fxgn.dev>
2023-08-24 18:53:11 +0300

    add line colors

---
+++ lib/data/lines.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+final class Lines {
+  static const lineColors = <int, Color>{
+    3: Color(0xff177bbb),
+    80: Color(0xfffbd500),
+    9: Color(0xffa9a8a8),
+    10: Color(0xffcbdd2c),
+    11: Color(0xff9fe4e2),
+    12: Color(0xffbac8e8),
+    14: Color(0xffd1281c),
+    15: Color(0xffeb86b7),
+    16: Color(0xfffaa81a),
+    17: Color(0xffef4d88),
+    1: Color(0xffD92128),
+    21: Color(0xff5fbee1),
+    4: Color(0xff5fbee1),
+    5: Color(0xff8a4c31),
+    6: Color(0xfff17e31),
+    7: Color(0xff9d3bb6),
+    8: Color(0xfffbd500),
+    2: Color(0xff4fc441),
+    20: Color(0xff9fe4e2),
+    18: Color(0xffea5b0c),
+  };
+}

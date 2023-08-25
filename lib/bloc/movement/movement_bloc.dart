db2077ed782ad1ff4a95f8052bcab3808573a000
flexagoon <code@fxgn.dev>
2023-08-25 10:28:33 +0300

    rename movement tracking event

--- lib/bloc/movement/movement_bloc.dart
+++ lib/bloc/movement/movement_bloc.dart
@@ -2,11 +2,11 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:mosmetro_wear/data/movement/movement_repository.dart';
 
-final class MovementBloc extends Bloc<StartMovementTrackingEvent, bool> {
+final class MovementBloc extends Bloc<StartMovementTracking, bool> {
   final MovementRepository repository;
 
   MovementBloc(this.repository) : super(false) {
-    on<StartMovementTrackingEvent>(
+    on<StartMovementTracking>(
       (event, emit) => emit.forEach(
         repository.movement,
         onData: (d) => d,
@@ -16,6 +16,6 @@ final class MovementBloc extends Bloc<StartMovementTrackingEvent, bool> {
 }
 
 @immutable
-final class StartMovementTrackingEvent {
-  const StartMovementTrackingEvent();
+final class StartMovementTracking {
+  const StartMovementTracking();
 }
